a390d843c6bdf1f0e5b48678d9ef4b6aa49ce660
Vincent Bedard-Legault <bedard_v@hotmail.fr>
2020-07-06 18:12:26 +0000

    added the randomString function to the POST method

--- express_server.js
+++ express_server.js
@@ -1,8 +1,11 @@
 const { request } = require("express");
+const bodyParser = require('body-parser')
 
 const express = require('express')
 const app = express()
-const PORT = 8080
+const PORT = 3001
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.set('view engine', 'ejs')
 
@@ -11,26 +14,31 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 }
 
-app.get('/', (req, res) => {
-  res.send('Hello!')
-})
+function generateRandomString(){
+  const chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+  var randomStr = '';
+  for (var i = 6; i > 0; i--) randomStr += chars[Math.round(Math.random() * (chars.length - 1))];
+  return randomStr;
+}
 
 app.get('/urls', (req, res) => {
   let templateVars = { urls: urlDatabase}
   res.render("urls_index", templateVars)
 })
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get('/urls/:shortURL', (req, res) => {
   let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]}
   res.render("urls_show", templateVars)
 })
 
-app.get('/urls.json', (req, res) => {
-  res.json(urlDatabase)
-})
-
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
+app.post("/urls", (req, res) => {
+  console.log(req.body);
+  console.log(res)
+  res.send(generateRandomString());
 });
 
 app.listen(PORT, () => {
