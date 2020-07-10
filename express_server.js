d05824a0fc9a96edd0162cba6b244c6c1ae33215
Vincent Bedard-Legault <bedard_v@hotmail.fr>
2020-07-10 15:47:04 +0000

    basic google oauth not perfect yet

--- express_server.js
+++ express_server.js
@@ -8,15 +8,31 @@ const redirectionRouter = require('./routers/redirection_router');
 
 const express = require('express');
 let methodOverride = require('method-override');
+
+const passport = require('passport');  //GOOGLEAUTH
+require('./passport-setup')           //GOOGLEAUTH
+
 const app = express();
 const PORT = 3001;
 
 app.use(methodOverride('_method'));
 app.use(bodyParser.urlencoded({extended: true}));
+
 app.use(cookieSession({
   name:'session',
   keys: ['key1', 'key2']
 }));
+
+const isLoggedIn = (req, res, next) => {
+  if(req.user){
+    next()
+  }else{
+    res.sendStatus(401)
+  }
+}
+
+app.use(passport.initialize())      //GOOGLEAUTH
+app.use(passport.session())         //GOOGLEAUTH
 
 app.set('view engine', 'ejs');
 
@@ -24,6 +40,25 @@ app.use('/', registrationRouter);
 app.use('/urls', urlRouter);
 app.use('/analytics', analyticsRouter);
 app.use('/', redirectionRouter);
+
+
+//GOOGLEAUTH
+app.get('/failed', (req, res) => res.send('You failed to log in'))
+app.get('/good', isLoggedIn, (req, res) => res.send(`Welcome mr ${req.user.profile}`))
+
+app.get('/google',
+  passport.authenticate('google', { scope: ['profile', 'email'] }));
+
+app.get('/google/callback', 
+  passport.authenticate('google', { failureRedirect: '/failed' }),
+  function(req, res) {
+    res.redirect('/good');
+  });
+  //GOOGLEAUTH
+
+  app.get('/*', (req, res) => {
+    res.render('404', {user:null})
+  })
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
---
+++ passport-setup.js
@@ -0,0 +1,23 @@
+const dotenv = require('dotenv')
+dotenv.config()
+const passport = require('passport')
+const GoogleStrategy = require('passport-google-oauth20').Strategy
+const users = require('./database')
+
+passport.serializeUser(function(user, done){
+  done(null, user)
+})
+
+passport.deserializeUser(function(user, done) {
+    done(null, user)
+  })
+
+passport.use(new GoogleStrategy({
+  clientID: process.env.GOOGLE_CLIENT_ID,
+  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  callbackURL: "http://localhost:3001/google/callback"
+},
+function(accessToken, refreshToken, profile, done){
+    return done(null, profile)
+  }
+))
